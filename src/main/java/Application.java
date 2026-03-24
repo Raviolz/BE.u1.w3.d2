61d03075373d19ecf03b41184d423658e868269c
Raviolz <gi.ravioli@gmail.com>
2026-03-24 19:06:56 +0100

    save.1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,19 @@
 import DAO.EventiDAO;
+import GestioneEventi.Entities.Evento;
+import GestioneEventi.Entities.TipoEvento;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.time.LocalDate;
+
 public class Application {
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("gestioneeventi");
+    // Apre la porta tra java e il database, connette i due mondi le informazioni passata verranno tradotte non da lui direttamente ma da EntityManager
+    // Persistence e' una classe JPA chje ha metodo createEntity ecc che serve a cercare le persintence unit .. quelkle che io ho scritto nel file
+
+    // QUELLA SINTASSI E' COME SE FOSSE UN NEW NASCOSTO CREA UN OGGETTO DI TIPO EMF
+
     // Per connettere l'applicazione al DB dobbiamo usare la Persistence Unit descritta dentro il file persistence.xml
     // Per farlo, creiamo un attributo statico di tipo EntityManagerFactory passandogli il nome della Persistence Unit
 
@@ -17,10 +26,26 @@ public class Application {
         // lo passo alla classe per poterlo usare .. creo un oggetto DAO che sa usare l EntityManager
         // perche mi serve per far si che l oggetto DAO sia solo un insieme di metodi per lavorare con DB
 
-        // quindi anche se e' un oggetto io gli posso passare altri oggetti come nel caso save perche lui
-        // lavora con dei metodi che per funzionare hanno bisogno di Entity Manager
+
         EventiDAO eventiDAO = new EventiDAO(entityManager);
 
         // questo e' possibile perche' di la nella classe ho un costruttore di questo tipo
+        // per poter poi usare i metodi che creo nella classe eventi dao mi bastera' riferirmi all oggetto eventiDAO. visto che ho appena creato questo oggetto che sa usare l EM
+
+
+        Evento e1 = new Evento("Matrimonio", LocalDate.parse("2026-08-22"), "Matrimonio tra Rosa e Pino a Villa Fabia in blu", TipoEvento.PRIVATO, 70);
+        Evento e2 = new Evento("Laurea", LocalDate.parse("2026-09-15"), " Laura Economia Gabriele", TipoEvento.PRIVATO, 40);
+        Evento e3 = new Evento("Presentazione Libro", LocalDate.parse("2026-04-01"), "Presentazione Libro per Bambini: L Efelante di Laura Bianchi", TipoEvento.PUBBLICO, 700);
+        Evento e4 = new Evento("Pensionamento Pino", LocalDate.parse("2026-05-29"), "Festa pensionamento Pino Astolfi anni 78", TipoEvento.PRIVATO, 120);
+
+        // per poter usare come detto sopra
+
+//        eventiDAO.save(e1);
+//        eventiDAO.save(e2);
+        eventiDAO.save(e3);
+        eventiDAO.save(e4);
+
+        // sarebbe poi da chiudere l entity maganer perche a quanto pare creandolo qui gia' e' aperto e non va bene tenerlo aperto spreca risorse ma tanto questa non e' app vera e a noi al momento non importa
+        // casomai poi comunque per riaprilo sarebbe da crearne uno nuovo come se quello vecchio fosse buttato via
     }
 }
--- src/main/java/DAO/EventiDAO.java
+++ src/main/java/DAO/EventiDAO.java
@@ -4,33 +4,35 @@ import GestioneEventi.Entities.Evento;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
+// classe che serve per lavorare con i database ci metto i metodi in modo che dal main siano accessibili senza sporcarlo con logica che qui rimane "nascosta"
+
 public class EventiDAO {
-    // attributo ..
+    // attributo ... serve epr parlare con il database per comandarlo
     private final EntityManager entityManager;
 
 
-    // costruttore
+    // costruttore serve peer poter passare l EM per poterci lavorare
     public EventiDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
 
-    // metodo ... APPUNTI ANCORA DA RIVEDERE PER ORA QUELLI DELLA LEZIONE
+    // Preparo metodi che poi verranno utilizzati nel main con logica nascosta. Qui e' solo definizione del metodo come al solito
 
+    // newEvento e' solo il nome del parametro da passargli non e' collegato al new della creazione potevo chiamarlo EventoInpuy
     public void save(Evento newEvento) {
-        // 1. Chiediamo all'Entity Manager di creare una nuova transazione
+        // 1. Chiedo all EM di aprire una transazione. E' il "posto" sicuro in cui lavorare. Qui dentro le operazioni sono tipo tutelate
         EntityTransaction transaction = this.entityManager.getTransaction();
 
-        // 2. Facciamo partire la transazione
+        // 2.Prima l avevo solo creata ma non iniziata
         transaction.begin();
 
-        // 3. Aggiungiamo newStudent (oggetto NON MANAGED) al PersistenceContext, facendolo
-        // quindi diventare MANAGED (cioè monitorato dall'EntityManager). Non sarà ancora parte
-        // del DB però
+        // 3 Dico che voglio salvare questo oggetto da qui diventa un oggetto controllato fino al commit rimarra' sotto osservazione fino a che decido la sua sorte
+        // se agigungerlo o meno al database e se prima modificarelo o no
+        // .persist(oggetto) serve per DIRE DI voler SALVARE ma ancora non lo fa
         entityManager.persist(newEvento);
 
-        // 4. Per far si che l'oggetto managed diventi una nuova riga della tabella dobbiamo
-        // fare il commit della transazione
+        // 4. Come commit git chiude e pusha nel database
         transaction.commit();
 
         // 5. Log di avvenuto salvataggio
